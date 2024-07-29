0fd35c4aaae5d446a6d4bcc2526a88cfb59754b4
Mkhue <mkhue47@gmail.com>
2024-07-29 09:13:36 +0200

    model type updates

--- models/contactNumber.go
+++ models/contactNumber.go
@@ -3,7 +3,7 @@ package models
 type ContactNumber struct {
 	MyModel
 	Id           int    `json:"id" gorm:"primary_key"`
-	UserId       int    `json:"userId" gorm:"nullable"`
+	UserId       int    `json:"userId"`
 	Type         string `json:"type"`
 	Number       string `json:"number"`
 	CountryCode  string `json:"countryCode"`
