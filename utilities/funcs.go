a5ba5c83e895a587c56ff943e6c015a1e6b6a8b6
Mkhue <mkhue47@gmail.com>
2024-08-12 12:11:42 +0200

    removed dotenv

--- utilities/funcs.go
+++ utilities/funcs.go
@@ -4,13 +4,12 @@ import (
 	"SleekSpace/dtos"
 	"SleekSpace/models"
 
-	"log"
+	// "log"
 	"math/rand"
 	"os"
 	"strconv"
 	"time"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func GenerateVerificationCode() int {
@@ -40,10 +39,10 @@ type EnvVariables struct {
 }
 
 func GetEnvVariables() EnvVariables {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file: %s", err)
+	// }
 	databaseDetails := os.Getenv("DATABASE_DETAILS")
 	email := os.Getenv("EMAIL")
 	emailPassword := os.Getenv("EMAIL_PASSWORD")
